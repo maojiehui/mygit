c129a59343469896398d68824f327f0d94574dca
mjh <523398927@qq.com>
2019-04-11 10:48:41 +0800

    李开伟的提交

--- src/main/java/com/qf/controller/UserInfoController.java
+++ src/main/java/com/qf/controller/UserInfoController.java
@@ -4,4 +4,6 @@ package com.qf.controller;
  * Created by DELL on 2019/4/11.
  */
 public class UserInfoController {
+
+
 }
--- src/main/java/com/qf/dao/UserInfoMapper.java
+++ src/main/java/com/qf/dao/UserInfoMapper.java
@@ -4,4 +4,6 @@ package com.qf.dao;
  * Created by DELL on 2019/4/11.
  */
 public interface UserInfoMapper {
+
+    public boolean addUserInfo();
 }

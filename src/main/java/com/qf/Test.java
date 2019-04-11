7cb969f11bc25afb4f33b625e8c02697dd215936
mjh <523398927@qq.com>
2019-04-11 10:35:06 +0800

    提交项目到github

--- src/main/java/com/qf/Test.java
+++ src/main/java/com/qf/Test.java
@@ -7,5 +7,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("哈哈");
+        System.out.println("wtf");
     }
 }
